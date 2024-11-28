31637d67d14b935d9948ff5cef3410432c129c56
Himidiri <himidirihimakanika@gmail.com>
2024-11-29 01:02:17 +0530

    Added MainController View

--- src/defensesystem/Starter.java
+++ src/defensesystem/Starter.java
@@ -4,9 +4,9 @@ package defensesystem;
  *
  * @author Himidiri Himakanika
  */
-public class DefenseSystem {
+public class Starter {
+
     public static void main(String[] args) {
         // TODO code application logic here
     }
-
 }
